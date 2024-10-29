3ca3b6fe3593dfb83eb863933fac401c869d23ef
Eric Olerud <eric@olerud.com>
2024-10-29 11:23:09 -0400

    Save file extensions as .csv

--- PingTable.ps1
+++ PingTable.ps1
@@ -24,7 +24,7 @@ while (1) {
     foreach ($Address in $Addresses) {
         $Result = Test-Address $Address
         if ($WriteToFile) {
-            $WriteLocation = Join-Path -Path $ResultDirectory -ChildPath $Address -
+            $WriteLocation = Join-Path -Path $ResultDirectory -ChildPath "$Address.csv"
             $Result | ConvertTo-CSv -NoHeader >> $WriteLocation
         } else {
             $Result | ConvertTo-CSv -NoHeader
