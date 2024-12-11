1d02063682494b70ae4f0b37a43cc52a784b1b9d
Eric Olerud <eric@olerud.com>
2024-12-11 15:55:11 -0500

    run pings async

--- PingTable.ps1
+++ PingTable.ps1
@@ -5,7 +5,10 @@ param (
 [int]$SleepTime = 5
 )
 
-function Test-Address {
+$cmd = {
+    param($Address, $WriteToFile, $ResultDirectory)
+
+    function Test-Address {
     param (
         [string]$Address
     )
@@ -14,6 +17,15 @@ function Test-Address {
     $Test = Test-NetConnection $Address
     $Result = @{"Address"=$Address; "Date"=$Date; "Success"=$Test.PingSucceeded; "ReplyTime"=$Test.PingReplyDetails.RoundTripTime}
     $Result
+    }
+
+    $Result = Test-Address $Address
+    if ($WriteToFile) {
+        $WriteLocation = Join-Path -Path $ResultDirectory -ChildPath "$Address.csv"
+        $Result | ConvertTo-CSv -NoHeader >> $WriteLocation
+    } else {
+        $Result | ConvertTo-CSv -NoHeader
+    }
 }
 
 if (-not (Test-Path -Path $ResultDirectory) -and $WriteToFile) {
@@ -21,13 +33,20 @@ if (-not (Test-Path -Path $ResultDirectory) -and $WriteToFile) {
 }
 
 while (1) {
+    # Clean up old jobs
+    Get-Job | Where-Object { $_.State -eq 'Completed' } | Remove-Job
+
     foreach ($Address in $Addresses) {
-        $Result = Test-Address $Address
-        if ($WriteToFile) {
-            $WriteLocation = Join-Path -Path $ResultDirectory -ChildPath "$Address.csv"
-            $Result | ConvertTo-CSv -NoHeader >> $WriteLocation
-        } else {
-            $Result | ConvertTo-CSv -NoHeader
+        Start-Job -ScriptBlock $cmd -ArgumentList $Address, $WriteToFile, $ResultDirectory -Name $Address > $null
+    }
+    # Wait for all jobs to finish
+    Get-Job | Wait-Job > $null
+    if (-not($WriteToFile)) {
+        foreach ($job in Get-Job) {
+            $output = Receive-Job -Job $job
+            if ($output) {
+                $output | Format-Table -AutoSize
+            }
         }
     }
     Start-Sleep -Seconds $SleepTime
